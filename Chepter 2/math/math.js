7b52312439eafdee3f73879870ca3a91ed3bc1af
Sarkar Nayan <web.sarkarnayan@gmail.com>
2025-08-24 11:09:45 +0600

    Math

--- Chepter 2/math/math.js
+++ Chepter 2/math/math.js
@@ -20,3 +20,7 @@ console.log(Math.pow(3, 5));
 // Number of Square Root
 console.log(Math.sqrt(9));
 console.log(Math.sqrt(64));
+
+// Number of Random
+console.log(Math.random());
+console.log(Math.round(Math.random() *50 + 1));
