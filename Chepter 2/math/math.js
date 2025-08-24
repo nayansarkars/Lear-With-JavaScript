aac85a923e2d354ead44b8be9bd4926dda9e21a4
Sarkar Nayan <web.sarkarnayan@gmail.com>
2025-08-24 11:05:44 +0600

    Math

--- Chepter 2/math/math.js
+++ Chepter 2/math/math.js
@@ -1,2 +1,22 @@
 console.log(Math.E);
 console.log(Math.PI);
+
+var n = 4.589;
+
+// Number of Absolute, Floor, Ceil, Round
+console.log(Math.abs(n));
+console.log(Math.floor(n));
+console.log(Math.ceil(n));
+console.log(Math.round(n));
+
+// Number of Max and Min
+console.log(Math.max(400, 500));
+console.log(Math.min(400, 500));
+
+// Number OF Power 
+console.log(Math.pow(2, 3));
+console.log(Math.pow(3, 5));
+
+// Number of Square Root
+console.log(Math.sqrt(9));
+console.log(Math.sqrt(64));
