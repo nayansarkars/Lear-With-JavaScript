c3177442b83a46223a3c29e06be402ff597fb57a
Sarkar Nayan <web.sarkarnayan@gmail.com>
2025-09-01 20:11:31 +0600

    JS Array

--- Chepter 2/js_jogic_&_conditions/js_if_condition/if.js
+++ Chepter 2/js_jogic_&_conditions/js_if_condition/if.js
@@ -1,1 +1,19 @@
 var a = 10;
+var b = 20;
+// a > b true or false
+
+/*
+if (Condition) {
+    all text
+}
+*/
+
+if (a > b) {
+    console.log("a is greater than b");
+}
+
+// if (a < b)
+
+if (b > a) {
+    console.log("b is greater than a");
+}
---
+++ Chepter 2/octal_hex.js
@@ -0,0 +1,5 @@
+var hex = 0xFF;
+console.log(hex); // 255
+
+var octal = 0o375;
+console.log(octal); // 255
